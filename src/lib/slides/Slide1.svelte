9ab88c7b090b9f232f02196b14baa8cffc320caa
Jovany <jovany.velez@gmail.com>
2024-07-28 09:43:00 -0500

    start

---
+++ src/lib/slides/Slide1.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Presentation, Slide } from '@animotion/core'
+</script>
+
+<Slide class="h-full ">
+
+    <p class="text-6xl font-bold drop-shadow-sm">🪄 Animotion</p>
+
+</Slide>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Presentation, Slide } from '@animotion/core'
+    import Slide1 from '$lib/slides/Slide1.svelte';
+</script>
+
+<Presentation options={{
+    transition: 'slide',
+    controls: true,
+    progress: true,
+    hash: true
+}}>
+	<Slide1 />
+  
+	<Slide class="h-full place-content-center place-items-center">
+		<p>Hola</p>
+        <Slide class="h-full place-content-center place-items-center">
+            <p>Mundo</p>
+            
+        </Slide>
+	</Slide>
+</Presentation>
